ac516c85b0dab8a8c55856a789652f045c006615
Adarsh Senghani <adarshco077@gmail.com>
2024-07-15 20:21:58 +0530

    feat: send result link only for specific branch

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-mod mongodb_ops;
-use sha2::{Digest, Sha256};
-
-fn get_sha256(html: String) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(html.as_bytes());
-    let result = hasher.finalize();
-
-    format!("{:X}", result)
-}
-
 async fn get_result_html() -> Option<String> {
-    match reqwest::get("").await {
+    match reqwest::get("http://www.mumresults.in").await {
         Ok(data) => match data.text().await {
             Ok(text) => Some(text),
             Err(err) => {
@@ -27,7 +16,11 @@ async fn get_result_html() -> Option<String> {
 
 async fn send_notification() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
-    client.post("").body("Site code updated!").send().await?;
+    client
+        .post("https://ntfy.sh/mumresult-test")
+        .body("http://www.mumresults.in/F24/1T01238.pdf")
+        .send()
+        .await?;
 
     Ok(())
 }
@@ -38,14 +31,7 @@ async fn main() -> mongodb::error::Result<()> {
 
     match maybe_html {
         Some(html) => {
-            let previous_sha_256 = mongodb_ops::get_saved_sha256().await?;
-
-            let new_sha_256 = get_sha256(html);
-
-            if previous_sha_256 != new_sha_256 {
-                // TODO: Notify
-                mongodb_ops::save_sha256(new_sha_256).await?;
-
+            if html.contains("/F24/1T01238") {
                 let _ = send_notification().await;
             }
         }
